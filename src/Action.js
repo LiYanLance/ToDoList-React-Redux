625955cd129d5da100e9387293aa0e488bcc5866
LiYanLance <yanxali@CNyanxali.local>
2018-09-13 17:09:04 +0800

    add store and default items

---
+++ src/Action.js
@@ -0,0 +1,14 @@
+const Action = {
+    STATUS: {
+        TODO : "TO DO",
+        IN_PROGRESS : "In Progress",
+        BLOCKED : "Blocked"
+    },
+    TAG: {
+        MEETING : "Meeting Review",
+        LEARNING : "learn skills",
+        PREPARATION : "preparation"
+    }
+}
+
+export default Action;
