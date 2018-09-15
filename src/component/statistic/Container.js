6627a4947f61530a0456395acef4222ab3e004fa
LiYanLance <yanxali@CNyanxali.local>
2018-09-15 16:49:04 +0800

    satistic chart

--- src/component/statistic/Container.js
+++ src/component/statistic/Container.js
@@ -1,18 +1,57 @@
 import Statistic from "./component/View";
 import {connect} from "react-redux"
+import Action from "../../Action";
 
-// const mapStateToProps = (state) => ({
-//     items: state
-// })
+const colorOfStatus = ["orange", "green", "purple"]
+const colorOfDueDate = [];
 
-// const mapDispatchToProps = dispatch => ({
-//     filter: name => dispatch({type:"FILTER_ITEM", name: name})
-// })
-//
-//
-// const mapStateToProps = (state) => ({
-//     items: state
-// })
+const getStatisticDataOfStatus = (state) => {
+    let map = new Map();
+    state.forEach(cur => {
+        const count = map.has(cur.status) ? map.get(cur.status) + 1 : 1;
+        map.set(cur.status, count)
+    });
+    let i = 0;
+
+    return Object.values(Action.STATUS)
+        .map(stat => ({title: stat, value: map.get(stat), color: colorOfStatus[i++]}))
+}
+
+const daysToDuedate = ["Out of date", "In 1 day", "In 3 days"];
+
+const getStatisticDataOfDueData = (state) => {
+    const toD = new Date();
+    let map = new Map();
+    state.forEach(cur => {
+        const diff = getDifferenceOfTwoDay(cur.dueDate, toD);
+        let key = "";
+        if (diff === 1) {
+            key = daysToDuedate[1];
+        } else if (diff <= 1) {
+            key = daysToDuedate[2];
+        } else {
+            key = daysToDuedate[0];
+        }
+        const count = map.has(key) ? map.get(key) + 1 : 1;
+        map.set(key, count)
+    });
+    let i = 0;
+
+    return daysToDuedate.map(days =>
+        ({title: days, value: map.get(days), color: colorOfStatus[i++]}))
+}
 
 
-export default connect()(Statistic)
+const getDifferenceOfTwoDay = (dueDate, today) => {
+    const dueD = new Date(dueDate.replace(new RegExp('-', 'g'), '/'));
+    const one_day = 1000 * 60 * 60 * 24;
+    const difference_ms = dueD.getTime() - today.getTime();
+    return Math.ceil(difference_ms / one_day);
+}
+
+const mapStateToProps = (state) => ({
+    statusOfActions: getStatisticDataOfStatus(state),
+    dueDateOfAction: getStatisticDataOfDueData(state)
+})
+
+export default connect(mapStateToProps)(Statistic)
--- src/component/statistic/component/View.js
+++ src/component/statistic/component/View.js
@@ -1,12 +1,73 @@
 import React, {Component} from "react";
+import PieChart from "react-svg-piechart"
+import "../style.css"
+import SearchBar from "../../search-bar";
 
 export default class ActionDialogView extends Component {
 
+    state = {
+        statusStatistic: null,
+        dueDateStatistic: null
+    }
+
+
     render() {
+        const data =
+
+            [
+                {title: "To Do", value: 3, color: "#22594e"},
+                {title: "In Progress", value: 5, color: "#2f7d6d"},
+                {title: "Blocked", value: 1, color: "#3da18d"},
+            ]
+
         return (
             <div>
-                statistic
+                <SearchBar/>
+                <div className="statistic-status">
+                    <h3>Status</h3>
+                    <PieChart data={this.props.statusOfActions} expandOnHover
+                              onSectorHover={(data) => {
+                                  if (data) {
+                                      this.displayCurrentStatusStatistic(data)
+                                  } else {
+                                      this.displayCurrentStatusStatistic()
+                                  }
+                              }}
+                    />
+                    <div>{this.state.statusStatistic}</div>
+                </div>
+                <div className="statistic-status">
+                    <h3>Duedate</h3>
+                    <PieChart data={this.props.dueDateOfAction} expandOnHover
+                              onSectorHover={(data) => {
+                                  if (data) {
+                                      this.displayCurrentDueDateStatistic(data)
+                                  } else {
+                                      this.displayCurrentDueDateStatistic()
+                                  }
+                              }}/>
+                    <div>{this.state.dueDateStatistic}</div>
+                </div>
             </div>
         )
     }
+
+    displayCurrentStatusStatistic(data) {
+        if (data) {
+            this.setState({statusStatistic: "" + data.title + " : " + data.value})
+        }
+        else {
+            this.setState({statusStatistic: ""});
+        }
+    }
+
+    displayCurrentDueDateStatistic(data) {
+        if (data) {
+            this.setState({dueDateStatistic: "" + data.title + " : " + data.value})
+        }
+        else {
+            this.setState({dueDateStatistic: ""});
+        }
+    }
+
 }
